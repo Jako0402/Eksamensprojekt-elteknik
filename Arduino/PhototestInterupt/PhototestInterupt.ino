4a79a3c1258a1cae59c2fcd8f44337f75cc1516a
Jakob <jakobjmhk@gmail.com>
2022-03-22 12:33:23 +0100

    Added interput on pin 3

--- Arduino/PhototestInterupt/PhototestInterupt.ino
+++ Arduino/PhototestInterupt/PhototestInterupt.ino
@@ -1,16 +1,25 @@
 const byte interruptPin = 2;
+const byte interruptPin2 = 3;
+
+
 volatile int count = 0; 
+volatile int count2 = 0; 
 
 void setup() {
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(interruptPin), add, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(interruptPin2), add2, CHANGE);
 }
 
 void loop() {
-  Serial.println(count);
+  Serial.println(count + " " + count2);
   delay(10);
 }
 
 void add() {
   count++;
+}
+
+void add2() {
+  count2++;
 }
