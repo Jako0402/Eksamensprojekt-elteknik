be7b5245773df28078ae61eba471f2db298de547
Jakob <jakobjmhk@gmail.com>
2022-03-22 11:19:41 +0100

    Uploaded TransceiverTest

---
+++ Arduino/TransceiverTest1/TestModtag/TestModtag.ino
@@ -0,0 +1,37 @@
+#include <SPI.h>
+#include "src/RF24/nRF24L01.h"
+#include "src/RF24/printf.h"
+#include "src/RF24/RF24.h"
+
+
+
+//create an RF24 object
+RF24 radio(7, 8);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+void setup()
+{
+  while (!Serial);
+    Serial.begin(9600);
+  
+  radio.begin();
+  
+  //set the address
+  radio.openReadingPipe(0, address);
+  
+  //Set module as receiver
+  radio.startListening();
+}
+
+void loop()
+{
+  //Read the data if available in buffer
+  if (radio.available())
+  {
+    char text[32] = {0};
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
---
+++ Arduino/TransceiverTest1/TestSend/TestSend.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include "src/RF24/nRF24L01.h"
+#include "src/RF24/printf.h"
+#include "src/RF24/RF24.h"
+
+
+
+//create an RF24 object
+RF24 radio(7, 8);  // CE, CSN
+
+//address through which two modules communicate.
+const byte address[6] = "00001";
+
+void setup()
+{
+  radio.begin();
+  
+  //set the address
+  radio.openWritingPipe(address);
+  
+  //Set module as transmitter
+  radio.stopListening();
+}
+void loop()
+{
+  //Send message to receiver
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  
+  delay(1000);
+}
